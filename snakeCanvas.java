b78de586260aef0614d913d2e06f4652fc745e11
Margarita <margarita@localhost.localdomain>
2016-02-27 18:25:14 +0200

    Drawing and position an objects - snake,fruit.

--- snakeCanvas.java
+++ snakeCanvas.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 /**
  * Created by margarita on 2/27/16.
  */
-public class snakeCanvas extends Canvas {
+public class snakeCanvas extends Canvas implements Runnable {
 
 
     private final int BOX_HEIGHT = 5;
@@ -15,14 +15,36 @@ public class snakeCanvas extends Canvas {
     private final int GRID_WEIGHT = 30;
 
     private LinkedList<Point> snake;
+    private Point fruit;
+
+    private Thread runThread;
+    private Graphics globalGraphycs;
+
+    public void Paint(Graphics g) {
+
+        snake =new LinkedList<Point>();
+        fruit = new Point();
+
+        globalGraphycs = g.create();
+        if (runThread == null) {
+            runThread = new Thread(this);
+            runThread.start();
+        }
+    }
 
     public void Draw(Graphics g) {
+        DrawGrid(g);
+        DrawSnake(g);
+        DrawFruit(g);
+    }
+
+    public void Move() {
 
     }
 
     public void DrawGrid(Graphics g) {
 
-        //drwaing an outside rectangle
+        //drawing an outside rectangle
         g.drawRect(0, 0, GRID_WEIGHT * BOX_WEIGHT, GRID_HEIGHT * BOX_HEIGHT);
 
         //drawing the vertical lines
@@ -34,7 +56,35 @@ public class snakeCanvas extends Canvas {
         for (int i = BOX_HEIGHT; i < GRID_HEIGHT * BOX_HEIGHT; i += BOX_HEIGHT) {
             g.drawLine(i, 0, i, BOX_WEIGHT * GRID_WEIGHT);
         }
-
     }
 
+    public void DrawSnake(Graphics g) {
+
+        g.setColor(Color.GREEN);
+        for (Point p : snake) {
+            g.fillRect(p.x, p.y, BOX_WEIGHT, BOX_HEIGHT);
+        }
+
+        g.setColor(Color.BLACK);
+    }
+
+    public void DrawFruit(Graphics g) {
+        g.setColor(Color.RED);
+        g.fillOval(fruit.x, fruit.y, BOX_WEIGHT, BOX_HEIGHT);
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            Move();
+            Draw(globalGraphycs);
+
+            try {
+                Thread.currentThread();
+                Thread.sleep(100);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 }
