803b942cae7f8cd1f379eadfe7507b290b21bcf8
Margarita <margarita@localhost.localdomain>
2016-02-29 10:47:25 +0200

    Fix syntax

--- SnakeApplet.java
+++ SnakeApplet.java
@@ -6,13 +6,13 @@ import java.awt.*;
 /**
  * Created by margarita on 2/28/16.
  */
-public class snakeApplet extends Applet {
+public class SnakeApplet extends Applet {
 
-    private snakeCanvas c;
+    private SnakeCanvas c;
 
     public void init() {
-        c = new snakeCanvas();
-        c.setPreferredSize(new Dimension(640,480));
+        c = new SnakeCanvas();
+        c.setPreferredSize(new Dimension(640, 480));
         c.setVisible(true);
         c.setFocusable(true);
         this.add(c);
@@ -20,7 +20,7 @@ public class snakeApplet extends Applet {
         this.setSize(new Dimension(640, 480));
     }
 
-    public void Paint(Graphics g) {
+    public void paint(Graphics g) {
         this.setSize(new Dimension(640, 480));
     }
 }
--- SnakeCanvas.java
+++ SnakeCanvas.java
@@ -10,13 +10,13 @@ import java.util.Random;
 /**
  * Created by margarita on 2/27/16.
  */
-public class snakeCanvas extends Canvas implements Runnable, KeyListener {
+public class SnakeCanvas extends Canvas implements Runnable, KeyListener {
 
 
     private final int BOX_HEIGHT = 15;
-    private final int BOX_WEIGHT = 15;
+    private final int BOX_WIDTH = 15;
     private final int GRID_HEIGHT = 25;
-    private final int GRID_WEIGHT = 25;
+    private final int GRID_WIDTH = 25;
 
     private LinkedList<Point> snake;
     private Point fruit;
@@ -28,13 +28,12 @@ public class snakeCanvas extends Canvas implements Runnable, KeyListener {
 
     private int score = 0;
 
-    public void Paint(Graphics g) {
+    public void paint(Graphics g) {
 
         this.setPreferredSize(new Dimension(640, 480));
         snake = new LinkedList<Point>();
-        GeneratedDefaultSnake();
-        PlaceFruit();
-        //fruit = new Point(10, 10);
+        generatedDefaultSnake();
+        placeFruit();
         this.addKeyListener(this);
 
         globalGraphycs = g.create();
@@ -44,7 +43,7 @@ public class snakeCanvas extends Canvas implements Runnable, KeyListener {
         }
     }
 
-    public void GeneratedDefaultSnake() {
+    public void generatedDefaultSnake() {
 
         score = 0;
         snake.clear();
@@ -55,22 +54,22 @@ public class snakeCanvas extends Canvas implements Runnable, KeyListener {
         direction = Direction.NO_DIRECTION;
     }
 
-    public void Draw(Graphics g) {
-        g.clearRect(0, 0, BOX_WEIGHT * GRID_WEIGHT + 10, BOX_HEIGHT * GRID_HEIGHT + 20); //for clear the screen if we dont the tail will not resize
-        //creat new image
-        BufferedImage buffer = new BufferedImage(BOX_WEIGHT * GRID_WEIGHT + 10, BOX_HEIGHT * GRID_HEIGHT + 20, BufferedImage.TYPE_INT_ARGB);
+    public void draw(Graphics g) {
+        g.clearRect(0, 0, BOX_WIDTH * GRID_WIDTH + 10, BOX_HEIGHT * GRID_HEIGHT + 20); //for clear the screen if we dont the tail will not resize
+        //create new image
+        BufferedImage buffer = new BufferedImage(BOX_WIDTH * GRID_WIDTH + 10, BOX_HEIGHT * GRID_HEIGHT + 20, BufferedImage.TYPE_INT_ARGB);
         Graphics bufferGraphics = buffer.getGraphics();
 
-        DrawSnake(bufferGraphics);
-        DrawGrid(bufferGraphics);
-        DrawFruit(bufferGraphics);
-        DrawScore(bufferGraphics);
+        drawSnake(bufferGraphics);
+        drawGrid(bufferGraphics);
+        drawFruit(bufferGraphics);
+        drawScore(bufferGraphics);
 
         //flip
-        g.drawImage(buffer, 0, 0, BOX_WEIGHT * GRID_WEIGHT + 10, BOX_HEIGHT * GRID_HEIGHT + 20, this);
+        g.drawImage(buffer, 0, 0, BOX_WIDTH * GRID_WIDTH + 10, BOX_HEIGHT * GRID_HEIGHT + 20, this);
     }
 
-    public void Move() {
+    public void move() {
 
         Point head = snake.peekFirst();
         Point newPoint = head;
@@ -88,13 +87,11 @@ public class snakeCanvas extends Canvas implements Runnable, KeyListener {
                 newPoint = new Point(head.x - 1, head.y);
                 break;
         }
-
         //remove the tail
         snake.remove(snake.peekLast());
 
         if (newPoint.equals(fruit)) {
 // the snake has hit fruit
-
             score += 10;
             Point addPoint = (Point) newPoint.clone();
 
@@ -113,73 +110,68 @@ public class snakeCanvas extends Canvas implements Runnable, KeyListener {
                     break;
             }
             snake.push(addPoint);
-            PlaceFruit();
-
-        } else if (newPoint.x < 0 || newPoint.x > (GRID_WEIGHT - 1)) {
-//we went oob
-            GeneratedDefaultSnake();
-
+            placeFruit();
+        } else if (newPoint.x < 0 || newPoint.x > (GRID_WIDTH - 1)) {
+            generatedDefaultSnake();
+            return;
         } else if (newPoint.y < 0 || newPoint.y > (GRID_HEIGHT - 1)) {
-            //we went oob
-            GeneratedDefaultSnake();
+            generatedDefaultSnake();
             return;
         } else if (snake.contains(newPoint)) {
-//we ran int ourself
-            GeneratedDefaultSnake();
+            //we ran int yourself
+            generatedDefaultSnake();
             return;
         }
-
         //if we reach this point the we are not dead
         snake.push(newPoint);
     }
 
-    public void DrawScore(Graphics g) {
+    public void drawScore(Graphics g) {
         g.drawString("Score: " + score, 0, BOX_HEIGHT * GRID_HEIGHT);
     }
 
-    public void DrawGrid(Graphics g) {
+    public void drawGrid(Graphics g) {
 
         //drawing an outside rectangle
-        g.drawRect(0, 0, GRID_WEIGHT * BOX_WEIGHT, GRID_HEIGHT * BOX_HEIGHT);
+        g.drawRect(0, 0, GRID_WIDTH * BOX_WIDTH, GRID_HEIGHT * BOX_HEIGHT);
 
         //drawing the vertical lines
-        for (int i = BOX_WEIGHT; i < GRID_WEIGHT * BOX_WEIGHT; i += BOX_WEIGHT) {
+        for (int i = BOX_WIDTH; i < GRID_WIDTH * BOX_WIDTH; i += BOX_WIDTH) {
             g.drawLine(i, 0, i, BOX_HEIGHT * GRID_HEIGHT);
         }
 
         //drawing the horizontal lines
         for (int i = BOX_HEIGHT; i < GRID_HEIGHT * BOX_HEIGHT; i += BOX_HEIGHT) {
-            g.drawLine(i, 0, i, BOX_WEIGHT * GRID_WEIGHT);
+            g.drawLine(i, 0, i, BOX_WIDTH * GRID_WIDTH);
         }
     }
 
-    public void DrawSnake(Graphics g) {
+    public void drawSnake(Graphics g) {
 
         g.setColor(Color.GREEN);
         for (Point p : snake) {
-            g.fillRect(p.x * BOX_WEIGHT, p.y * BOX_HEIGHT, BOX_WEIGHT, BOX_HEIGHT);
+            g.fillRect(p.x * BOX_WIDTH, p.y * BOX_HEIGHT, BOX_WIDTH, BOX_HEIGHT);
         }
 
         g.setColor(Color.BLACK);
     }
 
-    public void DrawFruit(Graphics g) {
+    public void drawFruit(Graphics g) {
         g.setColor(Color.RED);
-        g.fillOval(fruit.x * BOX_WEIGHT, fruit.y * BOX_HEIGHT, BOX_WEIGHT, BOX_HEIGHT);
+        g.fillOval(fruit.x * BOX_WIDTH, fruit.y * BOX_HEIGHT, BOX_WIDTH, BOX_HEIGHT);
         g.setColor(Color.BLACK);
     }
 
-    public void PlaceFruit() {
+    public void placeFruit() {
 
         Random rnd = new Random();
-        int randomX = rnd.nextInt(GRID_WEIGHT);
+        int randomX = rnd.nextInt(GRID_WIDTH);
         int randomY = rnd.nextInt(GRID_HEIGHT);
-
         Point rndPoint = new Point(randomX, randomY);
-        while (snake.contains(rndPoint)) {
-            randomX = rnd.nextInt(GRID_WEIGHT);
-            randomY = rnd.nextInt(GRID_HEIGHT);
 
+        while (snake.contains(rndPoint)) {
+            randomX = rnd.nextInt(GRID_WIDTH);
+            randomY = rnd.nextInt(GRID_HEIGHT);
             rndPoint = new Point(randomX, randomY);
         }
         fruit = rndPoint;
@@ -188,8 +180,8 @@ public class snakeCanvas extends Canvas implements Runnable, KeyListener {
     @Override
     public void run() {
         while (true) {
-            Move();
-            Draw(globalGraphycs);
+            move();
+            draw(globalGraphycs);
 
             try {
                 Thread.currentThread();
